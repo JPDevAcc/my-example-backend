69e981ee3c3358098614a76613aca18b9cb82483
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-02-27 14:06:59 +0000

    Use old routes instead

--- router.mjs
+++ router.mjs
@@ -5,8 +5,17 @@ const router = express.Router() ;
 // Routes
 router.get('/todo/:id', todos.show) ;
 router.get("/todo", todos.index) ;
+router.post("/create", todos.create) ;
+router.delete("/delete/:id", todos.remove) ;
+router.put("/todo/:id", todos.update) ;
+
+/*
+router.get('/todo/:id', todos.show) ;
+router.get("/todo", todos.index) ;
 router.post("/todo", todos.create) ;
 router.delete("/todo/:id", todos.remove) ;
 router.put("/todo/:id", todos.update) ;
+*/
+
 
 export default router ;
